210d55f3d0eb3f91acbabb0691e10638b6708db4
Felipe Garcia <felipegogarcia@gmail.com>
2026-09-14 08:41:30 -0300

    chore: add ReactPHP HTTP server entrypoint

---
+++ bin/server.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Psr\Http\Message\ServerRequestInterface;
+use React\Http\HttpServer;
+use React\Http\Message\Response;
+use React\Socket\SocketServer;
+
+$port = (int) ($_SERVER['PORT'] ?? 8080);
+
+$http = new HttpServer(static function (ServerRequestInterface $request): Response {
+    return Response::plaintext(
+        $request->getMethod() . ' ' . $request->getUri()->getPath() . "\n"
+    );
+});
+
+$http->listen(new SocketServer('0.0.0.0:' . $port));
+
+echo "Listening on http://0.0.0.0:{$port}\n";
